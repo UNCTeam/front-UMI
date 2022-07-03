5c29b24f542998745489e195f76b2fe94e64b6a0
Martin <martin.jourjon42@gmail.com>
2022-07-03 16:29:29 +0200

    ajout composants de base

--- Front-UMI/src/app/app.module.ts
+++ Front-UMI/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { UsersComponent } from './users/users.component';
+import { NavbarComponent } from './projects/navbar/navbar.component';
+import { ItemsComponent } from './projects/items/items.component';
+import { ModelsComponent } from './projects/models/models.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectsComponent,
+    UsersComponent,
+    NavbarComponent,
+    ItemsComponent,
+    ModelsComponent
   ],
   imports: [
     BrowserModule
